2d29d2da9cdd651ad6ca3e322ce4e88b4cb14667
Raúl Martín <raul.martin@urjc.es>
2021-06-01 19:23:26 +0200

    Uppercase classes, lowercase packages Format and rearrange all code

--- src/main/java/jnt/scimark2/SparseCompRow.java
+++ src/main/java/jnt/scimark2/SparseCompRow.java
@@ -1,18 +1,16 @@
 package jnt.scimark2;
 
-public class SparseCompRow
-{
-	/* multiple iterations used to make kernel have roughly
+public class SparseCompRow {
+	/* multiple iterations used to make Kernel have roughly
 		same granulairty as other Scimark kernels. */
 
-	public static double num_flops(int N, int nz, int num_iterations)
-	{
+    public static double num_flops(int N, int nz, int num_iterations) {
 		/* Note that if nz does not divide N evenly, then the
 		   actual number of nonzeros used is adjusted slightly.
 		*/
-		int actual_nz = (nz/N) * N;
-		return ((double)actual_nz) * 2.0 * ((double) num_iterations);
-	}
+        int actual_nz = (nz / N) * N;
+        return ((double) actual_nz) * 2.0 * ((double) num_iterations);
+    }
 
 
 	/* computes  a matrix-vector multiply with a sparse matrix
@@ -23,24 +21,21 @@ public class SparseCompRow
 		ith row in col[].  
 	*/
 
-	public static void matmult( double y[], double val[], int row[],
-		int col[], double x[], int NUM_ITERATIONS)
-	{
-		int M = row.length - 1;
+    public static void matmult(double[] y, double[] val, int[] row,
+							   int[] col, double[] x, int NUM_ITERATIONS) {
+        int M = row.length - 1;
 
-		for (int reps=0; reps<NUM_ITERATIONS; reps++)
-		{
-		
-			for (int r=0; r<M; r++)
-			{
-				double sum = 0.0; 
-				int rowR = row[r];
-				int rowRp1 = row[r+1];
-				for (int i=rowR; i<rowRp1; i++)
-					sum += x[ col[i] ] * val[i];
-				y[r] = sum;
-			}
-		}
-	}
+        for (int reps = 0; reps < NUM_ITERATIONS; reps++) {
+
+            for (int r = 0; r < M; r++) {
+                double sum = 0.0;
+                int rowR = row[r];
+                int rowRp1 = row[r + 1];
+                for (int i = rowR; i < rowRp1; i++)
+                    sum += x[col[i]] * val[i];
+                y[r] = sum;
+            }
+        }
+    }
 
 }
--- src/main/java/jnt/scimark2/Stopwatch.java
+++ src/main/java/jnt/scimark2/Stopwatch.java
@@ -1,114 +1,93 @@
 package jnt.scimark2;
 
 /**
-
-	Provides a stopwatch to measure elapsed time.
-
-Example of use:
-<pre>
-	Stopwatch Q = new Stopwatch;
-	Q.start();
-	//
-	// code to be timed here ...
-	//
-	Q.stop();
-	System.out.println("elapsed time was: " + Q.read() + " seconds.");
-</pre>	
-
-@author Roldan Pozo
-@version 14 October 1997, revised 1999-04-24
-*/
-public class Stopwatch 
-{
+ * Provides a stopwatch to measure elapsed time.
+ * <p>
+ * Example of use:
+ * <pre>
+ * Stopwatch Q = new Stopwatch;
+ * Q.start();
+ * //
+ * // code to be timed here ...
+ * //
+ * Q.stop();
+ * System.out.println("elapsed time was: " + Q.read() + " seconds.");
+ * </pre>
+ *
+ * @author Roldan Pozo
+ * @version 14 October 1997, revised 1999-04-24
+ */
+public class Stopwatch {
     private boolean running;
     private double last_time;
-	private double total;
+    private double total;
 
 
-/** 
-	Return system time (in seconds)
+    public Stopwatch() {
+        reset();
+    }
 
-*/
-	public final static double seconds()
-	{
-		return (System.currentTimeMillis() * 0.001);
-	}
-		
-/** 
-	Return system time (in seconds)
+    /**
+     * Return system time (in seconds)
+     */
+    public final static double seconds() {
+        return (System.currentTimeMillis() * 0.001);
+    }
 
-*/
-	public void reset() 
-	{ 
-		running = false; 
-		last_time = 0.0; 
-		total=0.0; 
-	}
-	
-    public Stopwatch()
-	{
-		reset();
-	}
-    
+    /**
+     * Return system time (in seconds)
+     */
+    public void reset() {
+        running = false;
+        last_time = 0.0;
+        total = 0.0;
+    }
 
-/** 
-	Start (and reset) timer
+    /**
+     * Start (and reset) timer
+     */
+    public void start() {
+        if (!running) {
+            running = true;
+            total = 0.0;
+            last_time = seconds();
+        }
+    }
 
-*/
-  	public  void start() 
-	{ 
-		if (!running)
-		{
-			running = true;
-			total = 0.0;
-			last_time = seconds(); 
-		}
-	}
-   
-/** 
-	Resume timing, after stopping.  (Does not wipe out
-		accumulated times.)
+    /**
+     * Resume timing, after stopping.  (Does not wipe out
+     * accumulated times.)
+     */
+    public void resume() {
+        if (!running) {
+            last_time = seconds();
+            running = true;
+        }
+    }
 
-*/
-  	public  void resume() 
-	{ 
-		if (!running)
-		{
-			last_time = seconds(); 
-			running = true;
-		}
-	}
-   
-   
-/** 
-	Stop timer
 
-*/
-   public  double stop()  
-	{ 
-		if (running) 
-        {
-			total += seconds() - last_time; 
+    /**
+     * Stop timer
+     */
+    public double stop() {
+        if (running) {
+            total += seconds() - last_time;
             running = false;
         }
-        return total; 
-    }
-  
- 
-/** 
-	Display the elapsed time (in seconds)
-
-*/
-   public  double read()   
-	{  
-		if (running) 
-       	{
-			total += seconds() - last_time;
-			last_time = seconds();
-		}
         return total;
-	}
-		
+    }
+
+
+    /**
+     * Display the elapsed time (in seconds)
+     */
+    public double read() {
+        if (running) {
+            total += seconds() - last_time;
+            last_time = seconds();
+        }
+        return total;
+    }
 }
 
     
